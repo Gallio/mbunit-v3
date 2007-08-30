2fd588fd32ad29bfeb95a066e9c58c7d5c8a797f
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2007-08-30 19:45:02 +0000

    Doh!  I renamed the files counter-convention to what I had first had in mind. Silly me.
    This will break the build momentarily.

--- src/MbUnit.AddIn.TDNet-Install.bat
+++ src/MbUnit.AddIn.TDNet-Install.bat
@@ -5,19 +5,19 @@ setlocal
 set SRCDIR=%~dp0
 set REG=%SRCDIR%..\libs\Tools\reg.exe
 set ICARUS_EXE=%SRCDIR%Runners\MbUnit.Icarus\bin\MbUnit.Icarus.exe
-set MBUNIT_ADDIN_DLL=%SRCDIR%Runners\MbUnit.AddIn\bin\MbUnit.AddIn.dll
+set MBUNIT_ADDIN_DLL=%SRCDIR%Runners\MbUnit.AddIn.TDNet\bin\MbUnit.AddIn.TDNet.dll
 set TDKEY=HKLM\Software\MutantDesign\TestDriven.Net\TestRunners
 
 set MbUnit2AddInPriority=20
 :MBUNIT2_RETRY_PROMPT
-set /P Answer=Use Gallio AddIn for MbUnit v2?  (Y/N)
+set /P Answer=Use Gallio TestDriven.Net Gallio AddIn for MbUnit v2?  (Y/N)
 if /I "%Answer%"=="Y" set MbUnit2AddInPriority=5 & goto :MBUNIT2_DONE_PROMPT
 if /I not "%Answer%"=="N" goto :MBUNIT2_RETRY_PROMPT
 :MBUNIT2_DONE_PROMPT
 
 set NUnitAddInPriority=20
 :NUNIT_RETRY_PROMPT
-set /P Answer=Use Gallio AddIn for NUnit?  (Y/N)
+set /P Answer=Use Gallio TestDriven.Net AddIn for NUnit?  (Y/N)
 if /I "%Answer%"=="Y" set NUnitAddInPriority=5 & goto :NUNIT_DONE_PROMPT
 if /I not "%Answer%"=="N" goto :NUNIT_RETRY_PROMPT
 :NUNIT_DONE_PROMPT
@@ -39,6 +39,6 @@ set FRAMEWORK=%~3
 %REG% ADD %TDKEY%\%NAME% /V Application /D %ICARUS_EXE% /F >nul
 %REG% ADD %TDKEY%\%NAME% /V AssemblyPath /D %MBUNIT_ADDIN_DLL% /F >nul
 %REG% ADD %TDKEY%\%NAME% /V TargetFrameworkAssemblyName /D %FRAMEWORK% /F >nul
-%REG% ADD %TDKEY%\%NAME% /V TypeName /D MbUnit.AddIn.MbUnitTestRunner /F >nul
+%REG% ADD %TDKEY%\%NAME% /V TypeName /D MbUnit.AddIn.TDNet.MbUnitTestRunner /F >nul
 goto :EOF
 
