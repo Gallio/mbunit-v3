7abcade4ffb95e8d2c6d80500fba6feefb200d16
julian.hidalgo <julian.hidalgo@06ccd1c7-0227-0410-afd2-95788076aca3>
2007-12-14 20:43:45 +0000

    Adding the projects for the future PowerShell Cmdlet.

---
+++ src/Gallio/Runners/Gallio.PowerShellCmdlet/GallioCmdlet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace Gallio.PowerShellCmdlet
+{
+    /// <summary>
+    /// A PowerShell Cmdlet for running Gallio.
+    /// </summary>
+    /// <todo>Implement</todo>
+    [Cmdlet("Run", "Gallio")]
+    public class GallioCmdlet : PSCmdlet
+    {
+        #region Private Members
+
+        private string filter;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// <include file='../../../Gallio/docs/FilterSyntax.xml' path='doc/summary/*' />
+        /// </summary>
+        /// <remarks>
+        /// <include file='../../../Gallio/docs/FilterSyntax.xml' path='doc/remarks/*' />
+        /// </remarks>
+        /// <example>
+        /// <include file='../../../Gallio/docs/FilterSyntax.xml' path='doc/example/*' />
+        /// </example>
+        [Alias("F")]
+        [Parameter]
+        public string Filter
+        {
+            set { filter = value; }
+        }
+
+        #endregion
+    }
+}
