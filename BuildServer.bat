8ba6ea1d172d30ed28625a6eafe9f073cff9a398
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2007-12-28 23:07:10 +0000

    More build server stuff.

--- BuildServer.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Builds everything for release and drops it in the Build folder.
-REM Includes building of documentation and other expensive activities.
-%~dp0\Build.bat /target:BuildServer
