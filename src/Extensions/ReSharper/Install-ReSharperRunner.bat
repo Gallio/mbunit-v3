8ffd8d7e1eaf289a67cfef93cf9a2de3a68691b5
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2008-03-22 10:02:44 +0000

    Modified this script to copy the plugin to Program Files in the same place as the installer puts it.  This avoids accidentally getting two copies of it installed.

--- src/Extensions/ReSharper/Install-ReSharperRunner.bat
+++ src/Extensions/ReSharper/Install-ReSharperRunner.bat
@@ -5,19 +5,19 @@ setlocal
 set LOCALDIR=%~dp0
 
 set RESHARPERRUNNER_DLL=%LOCALDIR%Gallio.ReSharperRunner\bin\Gallio.ReSharperRunner.dll
-set RESHARPERBASEDIR=%APPDATA%\JetBrains\ReSharper
+set RESHARPER_DIR=%PROGRAMFILES%\JetBrains\ReSharper
 
 echo Installing the locally compiled Gallio test runner for ReSharper.
 echo.
 
 :V30_VS80_RETRY_PROMPT
-set /P Answer=Support ReSharper v3.0 for VS 2005?  (Y/N)
+set /P Answer=Support ReSharper v3.1 for VS 2005?  (Y/N)
 if /I "%Answer%"=="Y" call :INSTALL "v3.1" "vs8.0" & goto :V30_VS80_DONE_PROMPT
 if /I not "%Answer%"=="N" goto :V30_VS80_RETRY_PROMPT
 :V30_VS80_DONE_PROMPT
 
 :V30_VS90_RETRY_PROMPT
-set /P Answer=Support ReSharper v3.0 for VS 2008?  (Y/N)
+set /P Answer=Support ReSharper v3.1 for VS 2008?  (Y/N)
 if /I "%Answer%"=="Y" call :INSTALL "v3.1" "vs9.0" & goto :V30_VS90_DONE_PROMPT
 if /I not "%Answer%"=="N" goto :V30_VS90_RETRY_PROMPT
 :V30_VS90_DONE_PROMPT
@@ -28,10 +28,16 @@ exit /b 0
 set RESHARPER_VERSION=%~1
 set VS_VERSION=%~2
 
-set PLUGINS_DIR=%RESHARPERBASEDIR%\%RESHARPER_VERSION%\%VS_VERSION%\Plugins
-set GALLIO_PLUGIN_DIR=%PLUGINS_DIR%\Gallio
+set RESHARPER_BIN_DIR=%RESHARPER_DIR%\%RESHARPER_VERSION%\%VS_VERSION%\Bin
+if not exist "%RESHARPER_BIN_DIR%" (
+    echo ReSharper %RESHARPER_VERSION% for %VS_VERSION% was not found.  Skipped.
+    goto :EOF
+)
 
-if not exist "%PLUGINS_DIR%" mkdir "%PLUGINS_DIR%"
+set RESHARPER_PLUGINS_DIR=%RESHARPER_BIN_DIR%\Plugins
+set GALLIO_PLUGIN_DIR=%RESHARPER_PLUGINS_DIR%\Gallio
+
+if not exist "%RESHARPER_PLUGINS_DIR%" mkdir "%RESHARPER_PLUGINS_DIR%"
 if not exist "%GALLIO_PLUGIN_DIR%" mkdir "%GALLIO_PLUGIN_DIR%"
 
 copy "%RESHARPERRUNNER_DLL%" "%GALLIO_PLUGIN_DIR%" /Y >nul
