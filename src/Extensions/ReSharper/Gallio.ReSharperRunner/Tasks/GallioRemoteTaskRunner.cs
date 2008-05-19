7dbb717553c4d10f19ab8825fb603df1bc8241f9
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2008-05-19 22:49:11 +0000

    Fixed a Release build problem with the R# v3.1 runner tests. Also pre-emptively added a couple of configuration options to improve test compatibility.

--- src/Extensions/ReSharper/Gallio.ReSharperRunner/Tasks/GallioRemoteTaskRunner.cs
+++ src/Extensions/ReSharper/Gallio.ReSharperRunner/Tasks/GallioRemoteTaskRunner.cs
@@ -14,6 +14,7 @@
 // limitations under the License.
 
 using System;
+using System.Threading;
 using Gallio.Loader;
 using Gallio.ReSharperRunner.Runtime;
 using Gallio.Runtime;
@@ -46,6 +47,7 @@ namespace Gallio.ReSharperRunner.Tasks
 
         public override void ConfigureAppDomain(TaskAppDomainConfiguration configuration)
         {
+            configuration.ApartmentState = ApartmentState.STA;
         }
 
         public override TaskResult Start(TaskExecutionNode node)
