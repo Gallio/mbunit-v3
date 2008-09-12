f08879a8d39cd038a0ba10ff252b822626d1b00e
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2008-09-12 16:50:34 +0000

    Added missing license boilerplate.

--- src/Extensions/VisualStudio/Gallio.VisualStudio.Sail/Properties/AssemblyInfo.cs
+++ src/Extensions/VisualStudio/Gallio.VisualStudio.Sail/Properties/AssemblyInfo.cs
@@ -1,4 +1,19 @@
-﻿using System.Reflection;
+// Copyright 2005-2008 Gallio Project - http://www.gallio.org/
+// Portions Copyright 2000-2004 Jonathan de Halleux
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -13,7 +28,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Gallio")]
-[assembly: AssemblyCopyright("Copyright © 2005-2008 Gallio Project - http://www.gallio.org/")]
+[assembly: AssemblyCopyright("Copyright � 2005-2008 Gallio Project - http://www.gallio.org/")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
