3adb7d2000b4160f86c998ac47f75f274a22e04e
grahamrhay <grahamrhay@06ccd1c7-0227-0410-afd2-95788076aca3>
2008-09-16 15:47:06 +0000

    Missed a few files.

---
+++ src/Extensions/Icarus/Gallio.Icarus.Tests/Controls/NodeCheckBoxTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using Aga.Controls.Tree;
+using Gallio.Icarus.Controls;
+using MbUnit.Framework;
+
+namespace Gallio.Icarus.Tests.Controls
+{
+    class NodeCheckBoxTest
+    {
+        [Test]
+        public void MouseDown_RightClick_Test()
+        {
+            NodeCheckBox nodeCheckBox = new NodeCheckBox();
+            nodeCheckBox.MouseDown(new TreeNodeAdvMouseEventArgs(new MouseEventArgs(MouseButtons.Right, 1, 0, 0, 0)));
+        }
+    }
+}
---
+++ src/Extensions/Icarus/Gallio.Icarus.Tests/Controls/ProjectTreeViewTest.cs
@@ -0,0 +1,28 @@
+﻿using Aga.Controls.Tree.NodeControls;
+using Gallio.Icarus.Controls;
+using MbUnit.Framework;
+
+namespace Gallio.Icarus.Tests.Controls
+{
+    class ProjectTreeViewTest
+    {
+        [Test]
+        public void Constructor_Test()
+        {
+            ProjectTreeView projectTreeView = new ProjectTreeView();
+            Assert.AreEqual(2, projectTreeView.NodeControls.Count);
+            
+            NodeIcon nodeIcon = (NodeIcon)projectTreeView.NodeControls[0];
+            Assert.AreEqual("Image", nodeIcon.DataPropertyName);
+            Assert.AreEqual(1, nodeIcon.LeftMargin);
+            Assert.IsNull(nodeIcon.ParentColumn);
+
+            NodeTextBox nodeTextBox = (NodeTextBox) projectTreeView.NodeControls[1];
+            Assert.AreEqual("Text", nodeTextBox.DataPropertyName);
+            Assert.IsTrue(nodeTextBox.IncrementalSearchEnabled);
+            Assert.IsFalse(nodeTextBox.EditEnabled);
+            Assert.AreEqual(3, nodeTextBox.LeftMargin);
+            Assert.IsNull(nodeTextBox.ParentColumn);
+        }
+    }
+}
