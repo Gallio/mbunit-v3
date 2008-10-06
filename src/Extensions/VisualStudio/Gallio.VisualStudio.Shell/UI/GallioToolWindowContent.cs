2aeeeb51219000b36dbedc6facb2a3e15e08cada
yann.trevin@gmail.com <yann.trevin@gmail.com@06ccd1c7-0227-0410-afd2-95788076aca3>
2008-10-06 05:20:33 +0000

    Added a missing file GallioToolWindowContent.cs (Oops!)

---
+++ src/Extensions/VisualStudio/Gallio.VisualStudio.Shell/UI/GallioToolWindowContent.cs
@@ -0,0 +1,59 @@
+﻿// Copyright 2005-2008 Gallio Project - http://www.gallio.org/
+// Portions Copyright 2000-2004 Jonathan de Halleux
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gallio.VisualStudio.Shell.UI
+{
+    /// <summary>
+    /// Abstract base class for components embedded in a <see cref="GallioToolWindowControl"/>.
+    /// </summary>
+    public abstract class GallioToolWindowContent : UserControl, IShellComponent
+    {
+        /// <summary>
+        /// Gets the shell associated with the component.
+        /// </summary>
+        public IShell Shell
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Default constructor.
+        /// (For the designer only)
+        /// </summary>
+        protected GallioToolWindowContent()
+        {
+        }
+
+        /// <summary>
+        /// Constructs a user control.
+        /// </summary>
+        /// <param name="shell">The shell associated with the component.</param>
+        protected GallioToolWindowContent(IShell shell)
+        {
+            if (shell == null)
+            {
+                throw new ArgumentNullException("shell");
+            }
+
+            this.Shell = shell;
+        }
+    }
+}
