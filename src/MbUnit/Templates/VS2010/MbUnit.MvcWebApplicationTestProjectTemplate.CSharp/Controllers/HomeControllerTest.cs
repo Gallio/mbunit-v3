893c0eeb01a84f71db246323dc269fa8fe10ca98
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2008-10-26 16:41:47 +0000

    Initial support for VS 2010.

---
+++ src/MbUnit/Templates/VS2010/MbUnit.MvcWebApplicationTestProjectTemplate.CSharp/Controllers/HomeControllerTest.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Gallio.Framework;
+using MbUnit.Framework;
+
+namespace $safeprojectname$.Controllers
+{
+    /// <summary>
+    /// Summary description for HomeControllerTest
+    /// </summary>
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Setup
+            HomeController controller = new HomeController();
+
+            // Execute
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Verify
+            ViewDataDictionary viewData = result.ViewData as ViewDataDictionary;
+
+            Assert.AreEqual("Home Page", viewData["Title"]);
+            Assert.AreEqual("Welcome to ASP.NET MVC!", viewData["Message"]);
+        }
+
+        [Test]
+        public void About()
+        {
+            // Setup
+            HomeController controller = new HomeController();
+
+            // Execute
+            ViewResult result = controller.About() as ViewResult;
+
+            // Verify
+            ViewDataDictionary viewData = result.ViewData as ViewDataDictionary;
+
+            Assert.AreEqual("About Page", viewData["Title"]);
+        }
+    }
+}
---
+++ src/MbUnit/Templates/VS2010/MbUnit.MvcWebApplicationTestProjectTemplate.VisualBasic/Controllers/HomeControllerTest.vb
@@ -0,0 +1,37 @@
+Imports System
+Imports System.Text
+Imports System.Collections.Generic
+Imports Gallio.Framework
+Imports MbUnit.Framework
+
+<TestFixture()> _
+Public Class HomeControllerTest
+    <Test()> _
+    Public Sub Index()
+        ' Setup
+        Dim controller As HomeController = New HomeController()
+
+        ' Execute
+        Dim result As ViewResult = controller.Index()
+
+        ' Verify
+        Dim viewData As ViewDataDictionary = result.ViewData
+
+        Assert.AreEqual("Home Page", viewData("Title"))
+        Assert.AreEqual("Welcome to ASP.NET MVC!", viewData("Message"))
+    End Sub
+
+    <Test()> _
+    Public Sub About()
+        ' Setup
+        Dim controller As HomeController = New HomeController()
+
+        ' Execute
+        Dim result As ViewResult = controller.About()
+
+        ' Verify
+        Dim viewData As ViewDataDictionary = result.ViewData
+
+        Assert.AreEqual("About Page", viewData("Title"))
+    End Sub
+End Class
