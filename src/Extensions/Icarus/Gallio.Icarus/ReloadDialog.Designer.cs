c02e1a608dbe72531b25b8800b5bacd8306589e8
grahamrhay <grahamrhay@06ccd1c7-0227-0410-afd2-95788076aca3>
2009-01-25 18:44:45 +0000

    Added icon to Reload dialog.

--- src/Extensions/Icarus/Gallio.Icarus/ReloadDialog.Designer.cs
+++ src/Extensions/Icarus/Gallio.Icarus/ReloadDialog.Designer.cs
@@ -110,6 +110,7 @@
             this.Controls.Add(this.assemblyLabel);
             this.Controls.Add(this.warningIcon);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ReloadDialog";
