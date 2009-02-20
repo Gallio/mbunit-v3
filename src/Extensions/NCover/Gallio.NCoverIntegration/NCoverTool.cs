37393005e938c55a1c8ea1991d3855eb3ffb87d2
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2009-02-20 02:43:01 +0000

    Disable temporary logging measure. Disable use of NCover on build server until issues can be resolved.

--- src/Extensions/NCover/Gallio.NCoverIntegration/NCoverTool.cs
+++ src/Extensions/NCover/Gallio.NCoverIntegration/NCoverTool.cs
@@ -38,8 +38,8 @@ namespace Gallio.NCoverIntegration
             ncoverArguments.Append(' ').Append(arguments);
             ncoverArguments.Append(" //w \"").Append(RemoveTrailingBackslash(workingDirectory)).Append('"');
             ncoverArguments.Append(" //x \"").Append(Path.Combine(workingDirectory, "Coverage.xml")).Append('"');
-            ncoverArguments.Append(" //l \"").Append(Path.Combine(workingDirectory, "Coverage.log")).Append('"');
-            ncoverArguments.Append(" //ll Verbose");
+            //ncoverArguments.Append(" //l \"").Append(Path.Combine(workingDirectory, "Coverage.log")).Append('"');
+            //ncoverArguments.Append(" //ll Verbose");
 
             return new ProcessTask(ncoverConsolePath, ncoverArguments.ToString(), workingDirectory);
         }
