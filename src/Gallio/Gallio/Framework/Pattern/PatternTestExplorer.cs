72195041ef3bdf62a9741b97fc49771fb9f5787b
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2009-04-28 19:50:00 +0000

    Fixed a typo that occurred in the last merge.

--- src/Gallio/Gallio/Framework/Pattern/PatternTestExplorer.cs
+++ src/Gallio/Gallio/Framework/Pattern/PatternTestExplorer.cs
@@ -40,13 +40,13 @@ namespace Gallio.Framework.Pattern
         public override bool IsTest(IReflectionPolicy reflectionPolicy, ICodeElementInfo codeElement)
         {
             var evaluator = CreateReflectionOnlyPatternEvaluator(reflectionPolicy);
-            return evaluator.IsTest(element, GetAutomaticPattern(element));
+            return evaluator.IsTest(element, GetAutomaticPattern(codeElement));
         }
 
         public override bool IsTestPart(IReflectionPolicy reflectionPolicy, ICodeElementInfo codeElement)
         {
             var evaluator = CreateReflectionOnlyPatternEvaluator(reflectionPolicy);
-            return evaluator.IsTestPart(element, GetAutomaticPattern(element));
+            return evaluator.IsTestPart(element, GetAutomaticPattern(codeElement));
         }
 
         public override void Explore(TestModel testModel, TestSource testSource, Action<ITest> consumer)
