3e085b74530ffbb9080ee3638170e61688291111
yann.trevin@gmail.com <yann.trevin@gmail.com@06ccd1c7-0227-0410-afd2-95788076aca3>
2009-05-25 10:34:26 +0000

    Added missing NamespaceDoc.cs in Gallio.Common.Time Synchronized VS2010 projects.

---
+++ src/Gallio/Gallio/Common/Time/NamespaceDoc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Gallio.Common.Time
+{
+    /// <summary>
+    /// The Gallio.Common.Time namespace contains various types to ease absolute and relative time management.
+    /// </summary>
+    [CompilerGenerated]
+    class NamespaceDoc
+    {
+    }
+}
