3f5f4dbc277b70a9ef946e35ab0871535b5a51f7
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2009-07-30 01:54:23 +0000

    Make sure we generate the XmlSerializers assembly for redistribution using .Net Framework 2.0.  In an annoying twist, it looks like the CodeDom APIs do not support selecting the target framework tools version so we had to move the code out of the MSBuild task to a separate process running under 2.0.

--- tools/Gallio.BuildTools/Gallio.BuildTools.Tasks/SGenMultipleTypes.cs
+++ tools/Gallio.BuildTools/Gallio.BuildTools.Tasks/SGenMultipleTypes.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Xml.Serialization;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
@@ -14,7 +11,7 @@ namespace Gallio.BuildTools.Tasks
     /// <summary>
     /// Generates an XmlSerializers assemblies for multiple types in an assembly.
     /// </summary>
-    public class SGenMultipleTypes : Task
+    public class SGenMultipleTypes : ToolTask
     {
         public string BuildAssemblyName { get; set; }
 
@@ -24,106 +21,53 @@ namespace Gallio.BuildTools.Tasks
 
         public string KeyFile { get; set; }
 
+        protected override string ToolName
+        {
+            get { return "Gallio.BuildTools.XsdGen.exe"; }
+        }
+
+        protected override string GenerateFullPathToTool()
+        {
+            string taskPath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            return Path.Combine(taskPath, ToolName);
+        }
+
+        protected override string GenerateCommandLineCommands()
+        {
+            StringBuilder result = new StringBuilder();
+
+            if (BuildAssemblyName != null)
+                result.Append("-an \"").Append(BuildAssemblyName).Append("\" ");
+
+            if (BuildAssemblyPath != null)
+                result.Append("-ap \"").Append(Path.GetFullPath(NormalizePath(BuildAssemblyPath))).Append("\" ");
+
+            foreach (string reference in References)
+                result.Append("-r \"").Append(reference).Append("\" ");
+
+            if (KeyFile != null)
+                result.Append("-k \"").Append(KeyFile).Append("\" ");
+
+            return result.ToString();
+        }
+
+        private static string NormalizePath(string path)
+        {
+            if (path.EndsWith("\\"))
+                return path.Substring(0, path.Length - 1);
+            return path;
+        }
+
         [Output]
         public string SerializationAssembly { get; private set; }
 
         public override bool Execute()
         {
-            // Use separate AppDomain so that we can unload the assemblies we generated
-            // the XmlSerializers from.  Otherwise when this task is used inside Visual Studio
-            // then the assembly will remain locked in memory.
-            AppDomain domain = AppDomain.CreateDomain("RemoteTask");
-            try
-            {
-                Type remoteTaskType = typeof(RemoteTask);
-                var remoteTask = (RemoteTask)domain.CreateInstanceFromAndUnwrap(
-                    new Uri(remoteTaskType.Assembly.CodeBase).LocalPath, remoteTaskType.FullName);
+            string absoluteAssemblyDir = Path.GetFullPath(BuildAssemblyPath);
+            string assemblyPath = Path.Combine(absoluteAssemblyDir, BuildAssemblyName);
+            SerializationAssembly = Path.ChangeExtension(assemblyPath, ".XmlSerializers.dll");
 
-                SerializationAssembly = remoteTask.Execute(BuildAssemblyPath, BuildAssemblyName, References, KeyFile);
-            }
-            finally
-            {
-                AppDomain.Unload(domain);
-            }
-
-            return true;
-        }
-
-        public sealed class RemoteTask : MarshalByRefObject
-        {
-            public string Execute(string relativeAssemblyDir, string assemblyFileName, string[] references, string keyFile)
-            {
-                string absoluteAssemblyDir = Path.GetFullPath(relativeAssemblyDir);
-                string assemblyPath = Path.Combine(absoluteAssemblyDir, assemblyFileName);
-
-                AppDomain.CurrentDomain.AssemblyResolve += (sender, e) =>
-                {
-                    // FIXME: Imprecise
-                    var requestedAssemblyName = new AssemblyName(e.Name).Name;
-                    if (! requestedAssemblyName.Contains("System"))
-                    {
-                        string requestedAssemblyDll = requestedAssemblyName + ".dll";
-
-                        foreach (string reference in references)
-                        {
-                            if (reference.ToLowerInvariant().Contains(requestedAssemblyDll.ToLowerInvariant()))
-                                return Assembly.LoadFrom(reference);
-                        }
-
-                        var candidateAssemblyPath = Path.Combine(absoluteAssemblyDir, requestedAssemblyDll);
-                        if (File.Exists(candidateAssemblyPath))
-                            return Assembly.LoadFrom(candidateAssemblyPath);
-                    }
-
-                    return null;
-                };
-                
-                string serializersAssemblyPath = Path.ChangeExtension(assemblyPath, ".XmlSerializers.dll");
-                Assembly assembly = Assembly.LoadFrom(assemblyPath);
-
-                List<Type> serializableTypes = new List<Type>();
-                foreach (Type type in assembly.GetExportedTypes())
-                {
-                    if (type.IsDefined(typeof(XmlRootAttribute), false))
-                        serializableTypes.Add(type);
-                }
-
-                List<XmlMapping> mappings = new List<XmlMapping>();
-                XmlReflectionImporter importer = new XmlReflectionImporter();
-
-                foreach (Type type in serializableTypes)
-                {
-                    XmlMapping mapping = importer.ImportTypeMapping(type);
-                    mappings.Add(mapping);
-                }
-
-                CompilerParameters compilerParameters = new CompilerParameters();
-
-                compilerParameters.TempFiles = new TempFileCollection();
-                compilerParameters.IncludeDebugInformation = false;
-                compilerParameters.GenerateInMemory = false;
-                compilerParameters.OutputAssembly = serializersAssemblyPath;
-
-                foreach (var reference in references)
-                {
-                    // FIXME: Imprecise
-                    if (! IsSystemAssembly(reference))
-                        compilerParameters.ReferencedAssemblies.Add(reference);
-                }
-
-                if (keyFile != null)
-                    compilerParameters.CompilerOptions += " /keyfile:\"" + keyFile + "\"";
-
-                XmlSerializer.GenerateSerializer(serializableTypes.ToArray(),
-                    mappings.ToArray(), compilerParameters);
-
-                return serializersAssemblyPath;
-            }
-        }
-
-        private static bool IsSystemAssembly(string assemblyName)
-        {
-            return assemblyName.Contains("System") || assemblyName.Contains("mscorlib");
+            return base.Execute();
         }
     }
 }
---
+++ tools/Gallio.BuildTools/Gallio.BuildTools.XsdGen/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Gallio.BuildTools.XsdGen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gallio")]
+[assembly: AssemblyCopyright("Copyright © 2005-2009 Gallio Project - http://www.gallio.org/")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7fa9a281-48e2-49e1-b5b1-91041c4f4f95")]
+
