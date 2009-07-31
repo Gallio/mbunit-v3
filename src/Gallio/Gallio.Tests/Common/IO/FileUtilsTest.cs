2188e3c2c51de33602e24577fc7ea7ecf2e37d3c
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2009-07-31 21:49:47 +0000

    Reimplemented FileUtils relative path functions so that we do not need to force the user to specify ".\" as a prefix on relative paths. Removed the dependency on NDepend.Helpers.FileDirectoryPath.dll.

---
+++ src/Gallio/Gallio.Tests/Common/IO/FileUtilsTest.cs
@@ -0,0 +1,87 @@
+// Copyright 2005-2009 Gallio Project - http://www.gallio.org/
+// Portions Copyright 2000-2004 Jonathan de Halleux
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gallio.Common.IO;
+using MbUnit.Framework;
+
+namespace Gallio.Tests.Common.IO
+{
+    [TestsOn(typeof(FileUtils))]
+    public class FileUtilsTest
+    {
+        [Test]
+        public void MakeAbsolutePath_WhenRelativePathIsNull_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => FileUtils.MakeAbsolutePath(null, "abc"));
+        }
+
+        [Test]
+        public void MakeAbsolutePath_WhenBasePathPathIsNull_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => FileUtils.MakeAbsolutePath(@"C:\abc", null));
+        }
+
+        [Test]
+        [Row(@"C.txt", @"C:\A\B", @"C:\A\B\C.txt")]
+        [Row(@"C.txt", @"C:\A\B\", @"C:\A\B\C.txt")]
+        [Row(@"..\..\C.txt", @"C:\A\B\", @"C:\C.txt")]
+        [Row(@"D:\X\Y\Z.txt", @"C:\A\B\", @"D:\X\Y\Z.txt")]
+        [Row(@"\\Machine\X\Y\Z.txt", @"\\Machine\X\W", @"\\Machine\X\Y\Z.txt")]
+        [Row(@"..\Y\Z.txt", @"\\Machine\X\W", @"\\Machine\X\Y\Z.txt")]
+        public void MakeAbsolutePath(string relativePath, string basePath, string expectedAbsolutePath)
+        {
+            string absolutePath = FileUtils.MakeAbsolutePath(relativePath, basePath);
+            Assert.AreEqual(expectedAbsolutePath, absolutePath);
+        }
+
+        [Test]
+        public void MakeRelativePath_WhenAbsolutePathIsNull_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => FileUtils.MakeRelativePath(null, "abc"));
+        }
+
+        [Test]
+        public void MakeRelativePath_WhenBasePathPathIsNull_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => FileUtils.MakeRelativePath(@"C:\abc", null));
+        }
+
+        [Test]
+        [Row(@"C:\A\B\C.txt", @"C:\", @"A\B\C.txt")]
+        [Row(@"C:\A\B\C.txt", @"C:\A\B\X", @"..\C.txt")]
+        [Row(@"C:\A\B\C.txt", @"C:\A\X\Y", @"..\..\B\C.txt")]
+        [Row(@"C:\A\B\C.txt", @"C:\A\B", @"C.txt")]
+        [Row(@"C:\A\B\C.txt", @"C:\A\B\", @"C.txt")]
+        [Row(@"C:\A\B\C.txt", @"C:\A", @"B\C.txt")]
+        [Row(@"C:\A\B\C.txt", @"C:\X", @"..\A\B\C.txt")]
+        [Row(@"C:\A\B\C.txt", @"C:\X\Y", @"..\..\A\B\C.txt")]
+        [Row(@"C:\A\B\C.txt", @"C:\X\Y\Z", @"..\..\..\A\B\C.txt")]
+        [Row(@"\\Machine\X\Y\Z.txt", @"C:\A\B\", @"\\Machine\X\Y\Z.txt")]
+        [Row(@"\\Machine\X\Y\Z.txt", @"\\Machine\X\W", @"..\Y\Z.txt")]
+        [Row(@"C:\A\B", @"C:\A\B", @".")]
+        [Row(@"C:\A\B\", @"C:\A\B\", @".")]
+        [Row(@"C:\A\B\", @"C:\A\B", @".")]
+        [Row(@"C:\A\B", @"C:\A\B", @".")]
+        public void MakeRelativePath(string absolutePath, string basePath, string expectedRelativePath)
+        {
+            string relativePath = FileUtils.MakeRelativePath(absolutePath, basePath);
+            Assert.AreEqual(expectedRelativePath, relativePath);
+        }
+    }
+}
