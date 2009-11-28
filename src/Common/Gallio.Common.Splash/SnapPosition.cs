8603bb6933bf9d0c6cb82ad8782680ac62652853
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2009-11-28 06:13:26 +0000

    Patched files that were messed up when I closed all files in Visual Studio.  (Seriously, wtf?  All files were saved.  Is ReSharper reverting renamed file buffers when closed?)

--- src/Common/Gallio.Common.Splash/SnapPosition.cs
+++ src/Common/Gallio.Common.Splash/SnapPosition.cs
@@ -7,7 +7,7 @@ namespace Gallio.Common.Splash
     /// <summary>
     /// Provides the result of mapping a screen position to a character index.
     /// </summary>
-    public struct Snap
+    public struct SnapPosition
     {
         private readonly SnapKind kind;
         private readonly int charIndex;
@@ -17,7 +17,7 @@ namespace Gallio.Common.Splash
         /// </summary>
         /// <param name="kind">The snap kind.</param>
         /// <param name="charIndex">The character index of the snap, or -1 if no snap.</param>
-        public Snap(SnapKind kind, int charIndex)
+        public SnapPosition(SnapKind kind, int charIndex)
         {
             this.kind = kind;
             this.charIndex = charIndex;
