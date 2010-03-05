c06b661cc974947c555671cdcb86d6a1beb6a768
yann.trevin <yann.trevin@06ccd1c7-0227-0410-afd2-95788076aca3>
2010-03-05 09:17:18 +0000

    Resigning the merged version of Gallio.dll.

--- src/Gallio/Gallio/ILMerge.bat
+++ src/Gallio/Gallio/ILMerge.bat
@@ -1,4 +1,4 @@
 CD %1
 COPY Gallio.dll Temp.dll
-..\..\..\..\tools\ILMerge\ILMerge.exe /out:Gallio.dll Temp.dll Microsoft.Cci.MetadataHelper.dll Microsoft.Cci.MetadataModel.dll Microsoft.Cci.PdbReader.dll Microsoft.Cci.PeReader.dll Microsoft.Cci.SourceModel.dll
+..\..\..\..\tools\ILMerge\ILMerge.exe /keyfile:..\..\..\Key.snk /out:Gallio.dll Temp.dll Microsoft.Cci.MetadataHelper.dll Microsoft.Cci.MetadataModel.dll Microsoft.Cci.PdbReader.dll Microsoft.Cci.PeReader.dll Microsoft.Cci.SourceModel.dll
 DEL Temp.dll
