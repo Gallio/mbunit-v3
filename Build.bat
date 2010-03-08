4e14f32d63e40f99c2445282d12e7ace12ecd7f4
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2010-03-08 20:45:48 +0000

    More work in progress on the new build system.

--- Build.bat
+++ Build.bat
@@ -8,6 +8,11 @@ setlocal
 set ROOT_DIR=%~dp0
 set MSBUILD_ARGS=%*
 
+if not defined MSBUILD_ARGS (
+  echo Must specify a module to build such as "packages\Bundle Package.module".
+  exit /b 1
+)
+
 if not defined MSBUILD set MSBUILD=%SystemRoot%\Microsoft.Net\Framework\v4.0.30128\MSBuild.exe
 if not defined MSBUILD set MSBUILD=%SystemRoot%\Microsoft.Net\Framework\v3.5\MSBuild.exe
 if not defined MSBUILD (
@@ -15,11 +20,25 @@ if not defined MSBUILD (
   exit /b 1
 )
 
-"%MSBUILD%" "%ROOT_DIR%\bin\Build.msbuild" /nologo /clp:NoSummary %MSBUILD_ARGS%
+call :SANITIZE ROOT_DIR
+
+"%MSBUILD%" /nologo /clp:NoSummary /p:"RootDir=%ROOT_DIR%" %MSBUILD_ARGS%
 
 if errorlevel 1 (
   echo Failed!
   exit /b 1
 )
 
+exit /b 0
+
+REM Sanitizes a path by converting it to a full path without a trailing backslash.
+REM We do this because the .Net command parser handles sequences like '\"' by taking the
+REM quote literally.  This causes problems when specifying MSBuild property values
+REM unless we remove or double-up the trailing backslash.
+:SANITIZE
+if not defined %~1 exit /b 0
+for /F "tokens=*" %%V in ('echo %%%~1%%') do set SANITIZE_TEMP=%%~dpnxV
+if "%SANITIZE_TEMP:~-1%"=="\" set SANITIZE_TEMP=%SANITIZE_TEMP:~0,-1%
+set %~1=%SANITIZE_TEMP%
+set SANITIZE_TEMP=
 exit /b 0
