dcb4fe4d8506e2f60cde78a11dd410e324083ea1
grahamrhay <grahamrhay@06ccd1c7-0227-0410-afd2-95788076aca3>
2010-10-28 21:12:25 +0000

    First draft of an Icarus Extension sample.

---
+++ src/Extensions/Icarus/Gallio.Icarus.ExtensionSample/Package.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Gallio.Icarus.ExtensionSample
+{
+    public class MyPackage : IPackage
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public void Load()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
