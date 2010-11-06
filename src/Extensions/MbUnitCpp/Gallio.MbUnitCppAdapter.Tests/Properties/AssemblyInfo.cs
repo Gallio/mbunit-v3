377d5333349f04448ca81862b011a0f4ae0fd1e8
yann.trevin <yann.trevin@06ccd1c7-0227-0410-afd2-95788076aca3>
2010-11-06 06:52:44 +0000

    [MbUnitCpp] Removed duplicate file/assembly version attributes.

--- src/Extensions/MbUnitCpp/Gallio.MbUnitCppAdapter.Tests/Properties/AssemblyInfo.cs
+++ src/Extensions/MbUnitCpp/Gallio.MbUnitCppAdapter.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gallio.MbUnitCppAdapter.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MbUnitCpp test adapter tests")]
+[assembly: AssemblyDescription("MbUnitCpp test adapter tests")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Gallio.MbUnitCppAdapter.Tests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MbUnitCpp")]
+[assembly: AssemblyCopyright("Copyright © 2005-2010 Gallio Project - http://www.gallio.org/")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d958b5c5-0aed-4fc8-845b-68796f298947")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
