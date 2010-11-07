5ea958029a54b56fc9134e0b51c5c4679a93988e
yann.trevin <yann.trevin@06ccd1c7-0227-0410-afd2-95788076aca3>
2010-11-07 16:57:04 +0000

    [MbUnitCpp] Added integration test foundations.

---
+++ src/Extensions/MbUnitCpp/Gallio.MbUnitCppAdapter.Tests/Integration/RunTest.cs
@@ -0,0 +1,52 @@
+﻿// Copyright 2005-2010 Gallio Project - http://www.gallio.org/
+// Portions Copyright 2000-2004 Jonathan de Halleux
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WC:\Projects\Gallio\v3\src\Extensions\MbUnitCpp\Gallio.MbUnitCppAdapter.Tests\Model\Bridge\UnmanagedTestRepositoryTest.csARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gallio.MbUnitCppAdapter.Model.Bridge;
+using Gallio.Model;
+using Gallio.Common.Reflection;
+using Gallio.Tests;
+using MbUnit.Framework;
+using System.Reflection;
+using System.IO;
+
+namespace Gallio.MbUnitCppAdapter.Tests.Integration
+{
+    [TestFixture, Pending("Not implemented")]
+    public class RunTest : BaseTestWithSampleRunner
+    {
+        [Column("x86"/*, "x64"*/)]
+        private readonly string architecture;
+
+        protected override void ConfigureRunner()
+        {
+            Runner.AddFile(new FileInfo(Helper.GetTestResources(architecture)));
+        }
+
+        [Test]
+        public void Empty_passes()
+        {
+            var runs = Report.TestPackageRun.AllTestStepRuns;
+
+
+        }
+
+
+    
+    }
+}
