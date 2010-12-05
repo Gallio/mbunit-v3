8b015cc9c639e470f6de39e9fe1295d9ea0362b0
grahamrhay <grahamrhay@06ccd1c7-0227-0410-afd2-95788076aca3>
2010-12-05 17:17:38 +0000

    Attempt at ExternalAnnotations for R#. Doesn't seem to work :(

--- src/Extensions/ReSharper/CopyFiles51.bat
+++ src/Extensions/ReSharper/CopyFiles51.bat
@@ -1,14 +1,16 @@
 @echo off
 
 @if defined PROGRAMFILES(x86) (
-set pluginFolder="%PROGRAMFILES(x86)%\JetBrains\ReSharper\v5.1\Bin\Plugins\Gallio"
+set installFolder=%PROGRAMFILES(x86)%\JetBrains\ReSharper\v5.1\Bin
 ) else (
-set pluginFolder="%PROGRAMFILES%\JetBrains\ReSharper\v5.1\Bin\Plugins\Gallio"
+set installFolder=%PROGRAMFILES%\JetBrains\ReSharper\v5.1\Bin
 )
+
+set pluginFolder="%installFolder%\Plugins\Gallio"
+set externalAnnotationsFolder="%installFolder%\ExternalAnnotations\MbUnit"
 
 md %pluginFolder%
 
 xcopy Gallio.ReSharperRunner\bin\v5.1\Gallio.ReSharperRunner51.dll %pluginFolder% /y
 xcopy Gallio.ReSharperRunner\bin\v5.1\Gallio.ReSharperRunner51.pdb %pluginFolder% /y
-
-pause
+xcopy Gallio.ReSharperRunner\MbUnit.xml %externalAnnotationsFolder% /y
