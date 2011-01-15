251cfc73b87ecdb0a86f525f1b3067c749e45cf7
grahamrhay <grahamrhay@06ccd1c7-0227-0410-afd2-95788076aca3>
2011-01-15 14:48:23 +0000

    Create MbUnit external annotations folder.

--- src/Extensions/ReSharper/CopyFiles51.bat
+++ src/Extensions/ReSharper/CopyFiles51.bat
@@ -10,6 +10,7 @@ set pluginFolder="%installFolder%\Plugins\Gallio"
 set externalAnnotationsFolder="%installFolder%\ExternalAnnotations\MbUnit"
 
 md %pluginFolder%
+md %externalAnnotationsFolder%
 
 xcopy Gallio.ReSharperRunner\bin\v5.1\Gallio.ReSharperRunner51.dll %pluginFolder% /y
 xcopy Gallio.ReSharperRunner\bin\v5.1\Gallio.ReSharperRunner51.pdb %pluginFolder% /y
