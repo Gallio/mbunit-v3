7fd31690ae74df2e5e6a58810e7f0131943ffb3f
yann.trevin <yann.trevin@06ccd1c7-0227-0410-afd2-95788076aca3>
2011-06-28 06:33:51 +0000

    Added [ExpectedInvalidOperationException] attribute.

---
+++ src/MbUnit/MbUnit/Framework/ExpectedInvalidOperationExceptionAttribute.cs
@@ -0,0 +1,54 @@
+// Copyright 2005-2010 Gallio Project - http://www.gallio.org/
+// Portions Copyright 2000-2004 Jonathan de Halleux
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Gallio.Framework.Pattern;
+
+namespace MbUnit.Framework
+{
+    /// <summary>
+    /// Declares that the associated test is expected to throw an <see cref="InvalidOperationException" />.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The expected contents of the exception message may optionally be specified.
+    /// </para>
+    /// </remarks>
+    [AttributeUsage(PatternAttributeTargets.Test, AllowMultiple = false, Inherited = true)]
+    public class ExpectedInvalidOperationExceptionAttribute : ExpectedExceptionAttribute
+    {
+        /// <summary>
+        /// Declares that the associated test is expected to throw an <see cref="InvalidOperationException" />.
+        /// </summary>
+        public ExpectedInvalidOperationExceptionAttribute()
+            : base(typeof(InvalidOperationException))
+        {
+        }
+
+        /// <summary>
+        /// Declares that the associated test is expected to throw an <see cref="InvalidOperationException" />.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The expected contents of the exception message may also optionally be specified.
+        /// </para>
+        /// </remarks>
+        /// <param name="message">The expected exception message, or null if not specified.</param>
+        public ExpectedInvalidOperationExceptionAttribute(string message)
+            : base(typeof(InvalidOperationException), message)
+        {
+        }
+    }
+}
