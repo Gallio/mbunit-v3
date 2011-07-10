943acca2e441e583fd4c3589ff7817dfe3596bfc
grahamrhay <grahamrhay@06ccd1c7-0227-0410-afd2-95788076aca3>
2011-07-10 11:19:07 +0000

    DRY.

--- src/Extensions/ReSharper/CopyFiles51.bat
+++ src/Extensions/ReSharper/CopyFiles51.bat
@@ -1,9 +1,11 @@
 @echo off
 
+set ReSharperVersion=v5.1
+
 @if defined PROGRAMFILES(x86) (
-set installFolder=%PROGRAMFILES(x86)%\JetBrains\ReSharper\v5.1\Bin
+set installFolder=%PROGRAMFILES(x86)%\JetBrains\ReSharper\%ReSharperVersion%\Bin
 ) else (
-set installFolder=%PROGRAMFILES%\JetBrains\ReSharper\v5.1\Bin
+set installFolder=%PROGRAMFILES%\JetBrains\ReSharper\%ReSharperVersion%\Bin
 )
 
 set pluginFolder="%installFolder%\Plugins\Gallio"
@@ -12,6 +14,6 @@ set externalAnnotationsFolder="%installFolder%\ExternalAnnotations\MbUnit"
 md %pluginFolder%
 md %externalAnnotationsFolder%
 
-xcopy Gallio.ReSharperRunner\bin\v5.1\Gallio.ReSharperRunner51.dll %pluginFolder% /y
-xcopy Gallio.ReSharperRunner\bin\v5.1\Gallio.ReSharperRunner51.pdb %pluginFolder% /y
+xcopy Gallio.ReSharperRunner\bin\%ReSharperVersion%\Gallio.ReSharperRunner51.dll %pluginFolder% /y
+xcopy Gallio.ReSharperRunner\bin\%ReSharperVersion%\Gallio.ReSharperRunner51.pdb %pluginFolder% /y
 xcopy Gallio.ReSharperRunner\MbUnit.xml %externalAnnotationsFolder% /y
