83cb4d286380aa1bcd526422e700faaafaec3c23
grahamrhay@gmail.com <grahamrhay@gmail.com@06ccd1c7-0227-0410-afd2-95788076aca3>
2011-10-11 13:43:05 +0000

    BinaryMessageFormatter test.

---
+++ src/Gallio/Gallio.Tests/Common/Messaging/BinaryMessageFormatterTests.cs
@@ -0,0 +1,23 @@
+﻿using Gallio.Common.Messaging;
+using Gallio.Model.Messages.Exploration;
+using MbUnit.Framework;
+using NHamcrest.Core;
+
+namespace Gallio.Tests.Common.Messaging
+{
+	public class BinaryMessageFormatterTests
+	{
+		[Test]
+		public void There_and_back_again()
+		{
+			var formatter = new BinaryMessageFormatter();
+			const string parentTestId = "parentTestId";
+			var message = new TestDiscoveredMessage { ParentTestId = parentTestId };
+
+			var bytes = formatter.Serialise(message);
+			var deserialisedMessage = (TestDiscoveredMessage)formatter.Deserialise(bytes);
+
+			Assert.That(deserialisedMessage.ParentTestId, Is.EqualTo(parentTestId));
+		}
+	}
+}
