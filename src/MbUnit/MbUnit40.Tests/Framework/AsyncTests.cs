140998b3c4d3a4a393306cf46ff0d357e4083acf
grahamrhay@gmail.com <grahamrhay@gmail.com@06ccd1c7-0227-0410-afd2-95788076aca3>
2012-02-15 22:20:10 +0000

    Async test attr. (Shamelessly stolen from XUnit).

---
+++ src/MbUnit/MbUnit40.Tests/Framework/AsyncTests.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace MbUnit.Framework.Tests
+{
+    public class AsyncTests
+    {
+        [AsyncTest]
+        public Task Pass()
+        {
+            return Task.Factory.StartNew(() => { });
+        }
+    }
+}
---
+++ src/MbUnit/MbUnit40/Framework/AsyncTestAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Gallio.Framework.Pattern;
+
+namespace MbUnit.Framework
+{
+    /// <summary>
+    /// Test async methods.
+    /// </summary>
+    public class AsyncTestAttribute : TestAttribute
+    {
+        /// <inheritdoc />
+        protected override void Execute(PatternTestInstanceState state)
+        {
+            var result = state.InvokeTestMethod();
+
+            var task = result as Task;
+            if (task != null)
+                task.Wait();
+        }
+    }
+}
